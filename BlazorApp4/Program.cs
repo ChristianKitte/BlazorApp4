1d11b49641af4d00c9c5cc5190a21f49b7586579
Christian Kitte <chkitte@web.de>
2023-12-10 09:29:53 +0100

    ss

--- BlazorApp4/Program.cs
+++ BlazorApp4/Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,11 +16,8 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
-
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
 app.Run();
